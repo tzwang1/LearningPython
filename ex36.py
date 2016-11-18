ca59dc498e904a696318630fbab623cb01f51217
Truman Wang <truman.wang@mail.utoronto.ca>
2016-11-18 10:54:35 -0500

    Edited ex36.py
    Saved character selection and stats

--- ex36.py
+++ ex36.py
@@ -1,10 +1,5 @@
 import random
 
-def start(start_choice):
-    if start_choice == "1":
-        select_character()
-
-
 def select_character():
     print "Choose your character:"
     print "Stinky"
@@ -18,20 +13,45 @@ def select_character():
     if choice == "Stinky":
         print " HP: 8\n ATK: 5\n DEF: 5\n SPD: 1"
         print " SPE: STENCH - Enemies and allies take 1 damage per turn"
+        _hp = 8
+        _atk = 5
+        _def = 5
+        _spd = 1
+        _spe = "STENCH"
 
     elif choice == "Binky":
         print " HP: 4\n ATK: 3\n DEF: 7\n SPD: 3"
         print " SPE: GREEDY - You gain double the gold after each battle"
+        _hp = 4
+        _atk = 3
+        _def = 7
+        _spd = 3
+        _spe = "GREEDY"
 
     elif choice == "Bloopy":
-        print " HP: 10\n ATK: %d\n DEF: %d\n SPD: %d" % (random.randint(1, 10), random.randint(1, 10), random.randint(1, 10))
+        _hp = 10
+        _atk = random.randint(1, 10)
+        _def = random.randint(1, 10)
+        _spd = random.randint(1, 10)
+        _spe = "BLOOP"
+        print " HP: 10\n ATK: %d\n DEF: %d\n SPD: %d" % (_atk, _def, _spd)
         print " SPE: BLOOP - Items you find may randomly change into other items of higher or lower value"
 
     elif choice == "Buppy":
         print " HP: 5\n ATK: 8\n DEF: 2\n SPD: 8"
-        print " SPE: Crit - Your attacks do double the damage"
+        print " SPE: CRIT - Your attacks do double the damage"
+        _hp = 5
+        _atk = 8
+        _def = 2
+        _spd = 8
+        _spe = "CRIT"
 
-    return choice
+    return choice, _hp, _atk, _def, _spd, _spe
+
+def start(start_choice):
+    if start_choice == "1":
+        character, _hp, _atk, _def, _spd, _spe = select_character()
+        print character, _hp, _atk, _def, _spd, _spe
 
 
 print "1. New Game"
